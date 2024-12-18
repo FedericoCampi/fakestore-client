2a9c4595a58ae1194bf7d0c8ab12f85532b50e55
FedericoCampi <federicocampi95@gmail.com>
2024-12-18 01:51:00 -0300

    readme

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -15,7 +15,7 @@ const ProductCard: React.FC<ProductProps> = ({ producto }) => {
   return (
     <a
     href={API_URL ? `/producto/${producto.id}` : undefined}
-      className="w-full flex flex-col justify-between items-center border rounded-lg p-4 shadow-md hover:shadow-lg hover:bg-gray-400"
+      className="w-full h-full flex flex-col justify-between items-center border rounded-lg p-4 shadow-md hover:shadow-lg hover:bg-gray-400"
     >
       <img src={producto.image} alt={producto.title} className="h-40 object-contain mx-auto" />
       <h2 className="mt-2 font-bold">{producto.title}</h2>
